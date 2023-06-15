4bd8f0e5e1e6c0b0de0fc1b7359685d067082f32
dangvanphuc <dangvanphuchd3@gmail.com>
2023-06-15 22:36:17 +0700

    BasePage WebDriver

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,103 @@
 package commons;
 
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
-	// Chứa những hàng dùng chung cho cả layer page objects
+	
+	public void openUrl(WebDriver driver, String url) {
+		driver.get(url);
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageSource(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public void acceptToAlert(WebDriver driver) {
+		waitForAlertPresence(driver).accept();
+	}
+	
+	public void cancelToAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss();
+	}
+	
+	public String getAlertText(WebDriver driver) {
+		return waitForAlertPresence(driver).getText();
+	}
+	
+	public void sendkeyToAlert(WebDriver driver, String valueToSendkey) {
+		waitForAlertPresence(driver).sendKeys(valueToSendkey);
+	}
+	
+	public Alert waitForAlertPresence(WebDriver driver) {
+		return new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void switchToWindowByID (WebDriver driver, String otherID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		
+		for (String id : allWindowIDs) {
+			if (!id.equals(otherID)) {
+				driver.switchTo().window(id);
+				sleepInSecond(2);
+			}
+		}
+	}
+	
+	public void switchToWindowByPageTitle(WebDriver driver, String expectedPageTitle) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		
+		for (String id : allWindowIDs) {
+			driver.switchTo().window(id);
+			
+			String actualTitle = driver.getTitle();
+			if (actualTitle.equals(expectedPageTitle)) {
+				break;
+			}
+		}
+	}
+	
+	public void closeAllWindownWithoutExpectedID(WebDriver driver, String expectedID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		
+		for (String id : allWindowIDs) {
+			if(!id.equals(expectedID)) {
+				driver.switchTo().window(id);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(expectedID);
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
