ae21f3660955c659c03d7edafabbb60691f1b02a
dangvanphuc <dangvanphuc@luvina.net>
2023-07-21 12:45:42 +0700

    Multiple Browser Lần III

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,18 +1,20 @@
 package commons;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseTest {
 	private WebDriver driver;
 	private String projectPath = System.getProperty("user.dir");
 	
 	protected WebDriver getBrowserDriver(String browserName) {
-		 if (browserName.equalsIgnoreCase("Chrome")) {
+		if (browserName.equalsIgnoreCase("Chrome")) {
 			 System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 			 driver = new ChromeDriver();
 		} else if (browserName.equalsIgnoreCase("Firefox")) {
@@ -21,13 +23,35 @@ public class BaseTest {
 		} else if (browserName.equalsIgnoreCase("Edge")){
 			 System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
 			 driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("Opera")){
+			 System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			 driver = new OperaDriver();
 		} else {
 			throw new RuntimeException("Browser name is not valid.");
 		}
+		 
+		switch (browserName) {
+		case "Chrome":
+			 System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			 break;
+		case "Firefox":
+			 System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			 driver = new FirefoxDriver();
+			 break;
+		default:
+			break;
+		}
 		
 		 driver.get("https://demo.nopcommerce.com/");
-		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		 return driver;
+	}
+	
+	protected String getEmailAddress() {
+		 String name = "johnwick";
+		 Random rad = new Random();
+		 return name + rad.nextInt(9999) + "@gmail.com";
 	}
 }
