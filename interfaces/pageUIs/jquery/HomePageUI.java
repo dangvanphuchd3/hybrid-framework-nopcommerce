80f48ac966b670aee829150e81795e87f596d4b1
dangvanphuc <dangvanphuc@luvina.net>
2023-09-11 19:04:24 +0700

    Topic 65: Handle DataTable Part I - 11/09/2023 19:04

---
+++ interfaces/pageUIs/jquery/HomePageUI.java
@@ -0,0 +1,8 @@
+package pageUIs.jquery;
+
+public class HomePageUI {
+	public static final String COLUMN_TEXTBOX_BY_NAME = "xpath=//div[text()='%s']/parent::div/following-sibling::input";
+	public static final String PAGE_LINK_BY_NUMBER = "xpath=//a[@class='qgrd-pagination-page-link' and text()='%s']";
+	public static final String PAGE_LINK_ACTIVE_BY_NUMBER = "xpath=//a[@class='qgrd-pagination-page-link active' and text()='%s']";
+	public static final String DYNAMIC_ROW_VALUES = "xpath=//td[@data-key='females' and text()='%s']/following-sibling::td[@data-key='country' and text()='%s']/following-sibling::td[@data-key='males' and text()='%s']/following-sibling::td[@data-key='total' and text()='%s']";
+}
--- testcases/com/jquery/table/Level_13_Handle_DataTable.java
+++ testcases/com/jquery/table/Level_13_Handle_DataTable.java
@@ -1,36 +1,78 @@
 package com.jquery.table;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.BaseTest;
+import pageObjects.jquery.HomePageObject;
+import pageObjects.jquery.PageGeneratorManager;
 
 public class Level_13_Handle_DataTable extends BaseTest {
 	 private WebDriver driver;
+	 HomePageObject homePage;
+	 
 	 
 	 @Parameters({"browser", "url"})
 	 @BeforeClass
 	 public void beforeClass(String browserName, String url) {
 		 driver = getBrowserDriver(browserName, url);
+		 
+		 homePage = PageGeneratorManager.getHomePage(driver);
 	 }
 	  
-	 @Test
-	 public void User_01_Register() {
-
+	 // @Test
+	 public void TC_01_Search() {
+		 // Search dữ liệu trong 1 Table (trên Header)
+		 homePage.inputToColumnTextboxByName("Females","750");
+		 homePage.sleepInSecond(2);
+		 
+		 homePage.inputToColumnTextboxByName("Males","651");
+		 homePage.sleepInSecond(2);
+		 
+		 homePage.inputToColumnTextboxByName("Country","Algeria");
+		 homePage.sleepInSecond(2);
+		 
+		 homePage.inputToColumnTextboxByName("Total","1900");
+		 homePage.sleepInSecond(2);
 	 }
 	 
-	 @Test
-	 public void User_02_Page_Navigation() {
-
+	 // @Test
+	 public void TC_02_Paging() {
+		 // Click to any page
+		 homePage.clickToPageByNumber("10");
+		 homePage.sleepInSecond(2);
+		 homePage.isPageActiveByNumber("10");
+		 
+		 homePage.clickToPageByNumber("12");
+		 homePage.sleepInSecond(2);
+		 homePage.isPageActiveByNumber("12");
+		 
+		 homePage.clickToPageByNumber("21");
+		 homePage.sleepInSecond(2);
+		 homePage.isPageActiveByNumber("21");
+		 
+		 homePage.clickToPageByNumber("20");
+		 homePage.sleepInSecond(2);
+		 homePage.isPageActiveByNumber("20");
 	 }
 	 
-	 @Test
-	 public void User_03_Page_Navigation() {
+	 // @Test
+	 public void TC_03_Displayed() {
+		 // Verify any row
+		 Assert.assertTrue(homePage.isRowValuesDisplayed("384187","Afghanistan","407124","791312"));
 		 
+		 Assert.assertTrue(homePage.isRowValuesDisplayed("750","Aruba","756","1504"));
 		 
+		 Assert.assertTrue(homePage.isRowValuesDisplayed("276880","Angola","276472","553353"));
+		 
+	 }
+	 
+	 public void TC_04_Icon_Button_Checkbox() {
+		 // Click vào bất kì 1 cái icon/ button/ checkbox/... của 1 row nào đó
 	 }
 	  
 	 @AfterClass
